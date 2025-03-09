eb28401381092c9ed4475fd7a347f451117c9e19
Lisandro Coronel <lisandrojulian2004@gmail.com>
2025-03-09 15:31:55 -0300

    cambie un pequeño error que no me permitia loguearme

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -9,26 +9,33 @@ import validarJWT from "../helpers/token-verify.js";
 
 const router = Router();
 
-// Ruta de login (pública)
-router.post("/", login);
+// Rutas públicas
+router
+  .route("/")
+  .post(login); // Inicio de sesión (no requiere JWT)
 
-// Ruta para crear un nuevo usuario (pública)
-router.post(
-  "/nuevo",
-  [
-    check("nombreUsuario").notEmpty().withMessage("El nombre es obligatorio"),
-    check("email", "El email es obligatorio").isEmail(),
-    check("password", "El password debe de ser de 8 caracteres")
-      .isLength({ min: 8, max: 15 })
-      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
-      .withMessage(
-        "El password debe contener 8 caracteres (al menos 1 letra mayúscula, 1 letra minúscula y 1 número). También puede incluir caracteres especiales"
-      ),
-  ],
-  crearUsuario
-);
+router
+  .route("/nuevo")
+  .post(
+    [
+      check("nombreUsuario").notEmpty().withMessage("El nombre es obligatorio"),
+      check("email", "El email es obligatorio").isEmail(),
+      check("password", "El password debe de ser de 6 caracteres")
+        .isLength({
+          min: 6,
+          max: 15,
+        })
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+        .withMessage(
+          "El password debe contener 8 caracteres (al menos 1 letra mayúscula, 1 letra minúscula y 1 numero) también puede incluir carácteres especiales"
+        ),
+    ],
+    crearUsuario
+  ); // Registro (no requiere JWT)
 
-// Ruta para listar usuarios (solo accesible para administradores)
-router.get([validarJWT], listarUsuarios);
+// Rutas protegidas
+router
+  .route("/")
+  .get([validarJWT], listarUsuarios); // Solo usuarios autenticados pueden listar usuarios
 
 export default router;
